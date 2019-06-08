36ffdae161f6f1d3a9d96f059d644f6ac04d9464
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2019-06-08 11:26:52 -0700

    Fixed arma prefixes

--- inst/include/ACTION/ACTION.h
+++ inst/include/ACTION/ACTION.h
@@ -23,6 +23,17 @@
 #include <ctime>
 
 
+#define ARMA_DONT_USE_WRAPPER
+#undef ARMA_BLAS_CAPITALS
+#define ARMA_BLAS_UNDERSCORE
+
+/*
+#undef ARMA_BLAS_CAPITALS
+#define ARMA_BLAS_UNDERSCORE
+#define ARMA_64BIT_WORD
+#define ARMA_BLAS_LONG_LONG
+*/
+
 #include <RcppArmadillo.h>
 #include <Rcpp.h>
 
